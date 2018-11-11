1937913c491d6d41f8be68f81786c998c5a238e4
kwang40 <kaishenw1995@gmail.com>
2018-11-11 17:46:16 -0600

    change ip addr

--- cameraServer.py
+++ cameraServer.py
@@ -9,12 +9,13 @@ server_socket.listen(0)
 
 # Accept a single connection and make a file-like object out of it
 connection = server_socket.accept()[0].makefile('rb')
-try:
     # Run a viewer with an appropriate command line. Uncomment the mplayer
     # version if you would prefer to use mplayer instead of VLC
-    cmdline = ['vlc', '--demux', 'h264', '-']
+cmdline = ['vlc', '--demux', 'h264', '-']
     #cmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
-    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+player = subprocess.Popen(cmdline, shell=True, stdin=subprocess.PIPE)
+try:
+
     while True:
         # Repeatedly read 1k of data from the connection and write it to
         # the media player's stdin
