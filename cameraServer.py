cb67abe99f4fa0c9c1ed7b1f93986a95a7dcdf1d
kwang40 <kaishenw1995@gmail.com>
2018-12-09 00:33:58 -0600

    change

--- cameraServer.py
+++ cameraServer.py
@@ -9,13 +9,12 @@ server_socket.listen(0)
 
 # Accept a single connection and make a file-like object out of it
 connection = server_socket.accept()[0].makefile('rb')
+try:
     # Run a viewer with an appropriate command line. Uncomment the mplayer
     # version if you would prefer to use mplayer instead of VLC
-#cmdline = ['vlc', '--demux', 'h264', '-']
-cmdline = ['mplayer', '-fps', '25', '-cache', '128', '-']
-player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
-try:
-
+    #cmdline = ['vlc', '--demux', 'h264', '-']
+    cmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
+    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
     while True:
         # Repeatedly read 1k of data from the connection and write it to
         # the media player's stdin
